505fd30cd6b5467945741d1e6aa87ea96d1accc7
ajauniskis <jauniskis.a@gmail.com>
2022-07-08 01:09:38 +0300

    creating db engine

---
+++ app/utils/database.py
@@ -0,0 +1,40 @@
+import os
+
+from sqlalchemy.engine import Engine
+from sqlalchemy import MetaData, create_engine
+from sqlalchemy.exc import OperationalError
+
+from utils.logger import logger
+
+
+def create_db_engine(
+    host: str | None = None,
+    database: str | None = None,
+    user: str | None = None,
+    password: str | None = None,
+) -> Engine:
+    try:
+        HOST = host or os.environ["POSTGRES_HOST"]
+        DATABASE = database or os.environ["POSTGRES_DB"]
+        USER = user or os.environ["POSTGRES_USER"]
+        PASSWORD = password or os.environ["POSTGRES_PASSWORD"]
+    except KeyError as e:
+        logger.info(f"Could not get database credentials: {e}")
+        raise KeyError(f"Could not get database credentials: {e}")
+
+    engine = create_engine(f"postgresql://{USER}:{PASSWORD}@{HOST}:5432/{DATABASE}")
+
+    try:
+        conn = engine.connect()
+        conn.close()
+    except OperationalError as e:
+        logger.error(f"Failed to create database engine: {e}")
+        raise e
+
+    logger.info("Database engine created")
+    return engine
+
+
+metadata = MetaData()
+
+engine = create_db_engine()
